b8d77cded5c67893ee4b1276479b5b7f3598ad28
gtongy <32930281+gtongy@users.noreply.github.com>
2018-08-25 21:33:00 +0900

    evaluator integer literal add

---
+++ evaluator/evaluator.go
@@ -0,0 +1,27 @@
+package evaluator
+
+import (
+	"github.com/gtongy/monkey/ast"
+	"github.com/gtongy/monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatement(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatement(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
